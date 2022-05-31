ba21a74ca2ad406411c3abc47937dd015d66256b
Guleid Magan <guleidmagan@guleids-mbp.lan>
2022-05-30 23:34:28 -0400

    Added Tailwind and CRA

---
+++ my-app/src/components/Header.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+
+const Header = () => {
+
+    return (
+        <h1 className="text-3xl font-bold underline">
+            Hello world!
+        </h1>
+    )
+    
+}
+
+export default Header
+
