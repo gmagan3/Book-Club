923accef7f339d27a1ae855195c2e0fc8401fd43
Guleid Magan <guleidmagan@guleids-mbp.lan>
2022-06-02 20:34:19 -0400

    Added tailwind

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header'
+import Books from './components/Books'
+
+const DUMMY_BOOKS = [
+  {
+    id: 'e1',
+    title: 'Lord of the Rings',
+    author: 'J.R.R. Tolkien',
+    rating: 5,
+  },
+  { 
+    id: 'e2', 
+    title: 'Game of Thrones', 
+    author: 'George RR Martin', 
+    rating: 5 },
+  {
+    id: 'e3',
+    title: 'Philosopher Stone',
+    author: 'J.K. Rowling',
+    rating: 5,
+  },
+
+];
 
 function App() {
   return (
--- my-app/src/components/Header.js
+++ my-app/src/components/Header.js
@@ -1,14 +1,23 @@
-import styled from 'styled-components'
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+
+
 
 const Header = () => {
-
     return (
-        <h1 className="text-3xl font-bold underline">
-            Hello world!
-        </h1>
-    )
-    
-}
+        <div>
+            <h1 className="text-3xl font-bold text-left underline">
+                Hello world!
+            </h1>
+
+        </div>
+        
+        
+    )}
 
 export default Header
-
